f044f1a65973f8138f3bd3b0bb2b673f7b446592
elephantLikesApple <elephant890@naver.com>
2023-03-13 16:23:37 +0900

    Compound operation

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,10 +2,16 @@ package org.example;
 
 public class Calc {
     public static int run(String exp) {
-        boolean needToMulti = exp.contains("*");
-        boolean needToSum = !exp.contains("*");
         exp = exp.replaceAll("\\- ", "\\+ \\-");
+        boolean needToMulti = exp.contains("*");
+        boolean needToSum = exp.contains("+");
+        boolean needToCompound = needToMulti && needToSum;
+
         int answer = 0;
+        if(needToCompound) {
+            String[] bits = exp.trim().split(" \\+ ");
+            return Integer.parseInt(bits[0]) + run(bits[1]);
+        }
         if(needToSum) {
             String[] bits = exp.trim().split(" \\+ ");
             for (int i = 0; i < bits.length; i++) {
